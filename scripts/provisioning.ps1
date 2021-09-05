efe1d9046fa1069495a74b40f7b1cc74491a2b8a
iwate <github@iwate.me>
2021-09-05 10:29:30 +0900

    modify provisioning script
    support version specific

--- scripts/provisioning.ps1
+++ scripts/provisioning.ps1
@@ -1,41 +1,58 @@
 param (
-    [Parameter(Mandatory=$true, HelpMessage = "Enter the name of the ScaleSet to which it belong.")]
+    [Parameter(ParameterSetName = "ManagedIdentity",  Mandatory=$true, HelpMessage = "Enter the name of the ScaleSet to which it belong.")]
+    [Parameter(ParameterSetName = "ServicePrincipal", Mandatory=$true, HelpMessage = "Enter the name of the ScaleSet to which it belong.")]
     [string] 
     $ScaleSetName,
 
-    [Parameter(Mandatory=$true, HelpMessage = "Enter azure subscription id of CosmosDB and KeyVault.")]
+    [Parameter(ParameterSetName = "ManagedIdentity",  Mandatory=$true, HelpMessage = "Enter azure subscription id of CosmosDB and KeyVault.")]
+    [Parameter(ParameterSetName = "ServicePrincipal",  Mandatory=$true, HelpMessage = "Enter azure subscription id of CosmosDB and KeyVault.")]
     [string] 
     $SubscriptionId,
 
-    [Parameter(Mandatory=$true, HelpMessage = "Enter resource group name of CosmosDB and KeyVault.")]
+    [Parameter(ParameterSetName = "ManagedIdentity",  Mandatory=$true, HelpMessage = "Enter resource group name of CosmosDB and KeyVault.")]
+    [Parameter(ParameterSetName = "ServicePrincipal",  Mandatory=$true, HelpMessage = "Enter resource group name of CosmosDB and KeyVault.")]
     [string] 
     $ResourceGroupName,
 
-    [Parameter(Mandatory=$true, HelpMessage = "Enter CosmosDB instance name.")]
+    [Parameter(ParameterSetName = "ManagedIdentity",  Mandatory=$true, HelpMessage = "Enter CosmosDB instance name.")]
+    [Parameter(ParameterSetName = "ServicePrincipal",  Mandatory=$true, HelpMessage = "Enter CosmosDB instance name.")]
     [string] 
     $CosmosDBName,
 
-    [Parameter(Mandatory=$true, HelpMessage = "Enter KeyVault instance name.")]
+    [Parameter(ParameterSetName = "ManagedIdentity",  Mandatory=$true, HelpMessage = "Enter KeyVault instance name.")]
+    [Parameter(ParameterSetName = "ServicePrincipal",  Mandatory=$true, HelpMessage = "Enter KeyVault instance name.")]
     [string] 
     $KeyVaultName,
 
-    [Parameter(Mandatory=$true, HelpMessage = "Enter tenant id of service principal.")]
+    [Parameter(ParameterSetName = "ServicePrincipal", Mandatory=$false, HelpMessage = "Enter tenant id of service principal.")]
     [string] 
     $AADTenantId,
 
-    [Parameter(Mandatory=$true, HelpMessage = "Enter client id of service principal.")]
+    [Parameter(ParameterSetName = "ServicePrincipal", Mandatory=$false, HelpMessage = "Enter client id of service principal.")]
     [string] 
     $AADClientId,
 
-    [Parameter(Mandatory=$true, HelpMessage = "Enter client secret of service principal.")]
+    [Parameter(ParameterSetName = "ServicePrincipal", Mandatory=$false, HelpMessage = "Enter client secret of service principal.")]
     [string] 
     $AADClientSecret
+
+    [Parameter(ParameterSetName = "ManagedIdentity",  Mandatory=$false, HelpMessage = "Enter SaburaIIS release version.")]
+    [Parameter(ParameterSetName = "ServicePrincipal",  Mandatory=$false, HelpMessage = "Enter SaburaIIS release version.")]
+    [string] 
+    $Version = "latest"
 )
 
 $ErrorActionPreference = 'Stop'
 
+# Resolve download link
+$DownloadLink = "https://github.com/iwate/saburaiis/releases/latest/download/SaburaIIS.Agent.zip"
+
+if ($Version -ne "latest") {
+    $DownloadLink = "https://github.com/iwate/saburaiis/releases/download/$Version/SaburaIIS.Agent.zip"
+}
+
 # Download agent binary
-Invoke-RestMethod -Method Get -Uri "https://github.com/iwate/saburaiis/releases/latest/download/SaburaIIS.Agent.zip" -OutFile $env:temp\saburaiis.zip
+Invoke-RestMethod -Method Get -Uri $DownloadLink -OutFile $env:temp\saburaiis.zip
 Expand-Archive -Path $env:temp\saburaiis.zip -DestinationPath $env:SystemDrive\saburaiis
 
 # Create service
