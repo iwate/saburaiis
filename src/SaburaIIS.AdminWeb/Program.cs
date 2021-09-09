a17537f2bb6329451fbe56242354e8b476982853
iwate <github@iwate.me>
2021-09-09 23:39:28 +0900

    cleanup bootstap

--- src/SaburaIIS.AdminWeb/Program.cs
+++ src/SaburaIIS.AdminWeb/Program.cs
@@ -1,20 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using SaburaIIS.AdminWeb;
 
-namespace SaburaIIS.AdminWeb
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(o => o.UseStartup<Startup>())
+    .Build()
+    .Run();
