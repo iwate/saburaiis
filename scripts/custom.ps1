d87231c105cc8e6802ee026a114a79f7194d4141
iwate <github@iwate.me>
2021-09-21 23:13:16 +0900

    fix path

--- scripts/custom.ps1
+++ scripts/custom.ps1
@@ -1,7 +1,7 @@
 param(
     $DotNetCoreDownloadLink = "https://download.visualstudio.microsoft.com/download/pr/c154f72b-dcaf-4852-8fbe-20f0f3c779a7/91c7e664a755fb3142740f14c5c96ea7/dotnet-hosting-3.1.19-win.exe",
     $FunctionsDownloadLink = "https://github.com/Azure/azure-functions-host/releases/download/v3.2.0/Functions.3.2.0.zip",
-    $FunctionsRuntimePath = "c:\inetpub\site\.functions"
+    $FunctionsRuntimePath = "c:\inetpub\sites\.functions"
 )
 $ErrorActionPreference = "Stop"
 
